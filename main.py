5aed2764c7adf0a69d7708ba6b80f0e1cd29a63d
Hasanal Bulkiah <hasanal.bulkiah501@gmail.com>
2025-07-29 23:48:09 +0700

    create main for running ingestion process

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import os
 from utils.database import Database
 from script.ingestion import Ingestion
 
-db = Database("postgresql", "localhost", 5432, "postgres", "postgres", "dvdrental")
-ingestion = Ingestion(
-                db, 
-                os.environ["MINIO_ENDPOINT"], 
-                os.environ["MINIO_ACCESS_KEY"], 
-                os.environ["MINIO_SECRET_KEY"], 
-                os.environ["MINIO_SECURE"], 
-                os.environ["MINIO_BUCKET_NAME"]
-            )
+db = Database()
+ingestion = Ingestion(db)
+ingestion.extract()
